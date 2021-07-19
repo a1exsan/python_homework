33fb4c1f359f6590b04968286cfc1bdcbff8ad69
alex <fominsan@gmail.com>
2021-07-19 12:50:30 +0700

    added exercise 5 from lesson 2 correction

--- lesson2_ex5.py
+++ lesson2_ex5.py
@@ -4,21 +4,8 @@ rating = [8,7,7,5]
 
 num = int(input('Enter number 0 - 9: '))
 
-rating.reverse()
-
-if len(rating) == 0:
-    rating.append(num)
-else:
-    if num in rating:
-        index = rating.index(num)
-        rating.insert(index, num)
-    else:
-        if num >= max(rating):
-            rating.append(num)
-        else:
-            rating.insert(0, num)
-
-rating.reverse()
+rating.append(num)
+rating.sort(reverse=True)
 
 print(f'rating is: ', end = '')
 
