b88057059b924f861085eeed88353f59cbaa3051
alex <fominsan@gmail.com>
2021-07-19 11:05:16 +0700

    added exercise 4 from lesson 2

---
+++ lesson2_ex4.py
@@ -0,0 +1,13 @@
+# written by: Alex fominsan@gmail.com
+
+input_str = input('Enter any sentence devided by space: ')
+
+input_list = input_str.split(' ')
+
+for i in range(len(input_list)):
+    print(i + 1, f' {input_list[i][: (10, len(input_list[i]))[len(input_list[i]) <= 10]]}')
+    #if len(input_list[i]) > 10:
+    #    print(i + 1, f' {input_list[i][:10]}')
+    #else:
+    #    print(i + 1, f' {input_list[i]}')
+
