3cc1a87412ebef2e06bae361f2a62e4eea2a1ab5
alex <fominsan@gmail.com>
2021-07-20 22:31:54 +0700

    added exercise 5 from lesson 2 correct sorting procedure

--- lesson2_ex5.py
+++ lesson2_ex5.py
@@ -4,8 +4,15 @@ rating = [8,7,7,5]
 
 num = int(input('Enter number 0 - 9: '))
 
-rating.append(num)
-rating.sort(reverse=True)
+#rating.append(num)
+#rating.sort(reverse=True)
+
+for i in range(len(rating)):
+    if num > rating[i]:
+        rating.insert(i, num)
+        break
+    elif i == len(rating) - 1:
+        rating.append(num)
 
 print(f'rating is: ', end = '')
 
