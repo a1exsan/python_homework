ca6201262732c4a5e749db2c0770f2168e784ede
alex <fominsan@gmail.com>
2021-07-23 10:48:25 +0700

    added exercise 6 from lesson 3

--- lesson3/lesson3_ex6.py
+++ lesson3/lesson3_ex6.py
@@ -1,3 +1,16 @@
 # written by: Alex fominsan@gmail.com
 
+def int_func(input_str):
+    sl = list(input_str)
+    sl[0] = sl[0].upper()
+    return ''.join(sl)
 
+def main():
+    user_input = input('Enter sentence splitted by space: ').split(' ')
+
+    print(user_input)
+
+    for ui in user_input:
+        print(f'{int_func(ui)} ', end = '')
+
+main()
